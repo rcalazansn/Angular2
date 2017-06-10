8ac58b8a918799af9d98e0ced4acf03b9016d4fa
ruby --v <Rodrigo>
2017-06-10 16:51:10 -0300

    adicionado projeto ao GIT

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'meu-primeiro-component1',
+    template: `
+        <p>Meu primeiro comeponent com Angular 2!</p>
+    `
+})
+export class MeuPrimeiroComponent  {}
