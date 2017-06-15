45002661b8accb79921b29a83ad699bdf188d316
ruby --v <Rodrigo>
2017-06-15 20:28:03 -0300

    criação de modulos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
+
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
